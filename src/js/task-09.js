af98321dccd57d51142d90d26db4ba170984cc46
VladaHrazhdiian <123592785+VladaHrazhdiian@users.noreply.github.com>
2023-06-03 20:10:51 +0300

    work

---
+++ src/js/task-09.js
@@ -0,0 +1,17 @@
+const colorButton = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const bodyElement = document.body;
+
+colorButton.addEventListener('click', changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const randomColor = getRandomHexColor();
+  bodyElement.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
